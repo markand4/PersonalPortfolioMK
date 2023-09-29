62231b0ebf857d018cd626d8290188a51a175a82
Mark Kurpiel <MarkKurpiel@gmail.com>
2023-09-29 16:36:57 -0700

    made resume responsive

--- components/Resume.js
+++ components/Resume.js
@@ -6,7 +6,7 @@ export default function Resume() {
     <section>
       <div className="relative z-10 rounded-md shadow-md bg-[#202A44] p-4 md:p-10 lg:p-20 max-w-6xl mx-auto mb-20 -mt-4">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <iframe src="/MarkKurpielResume.pdf" style={{ height: '100vh', width: '100vh' }}/>
+            <iframe className="w-full aspect-video" src="/MarkKurpielResume.pdf" style={{ height: '100vh', width: '100vh' }}/>
         </div>
       </div>
     </section>
