d1334afb1375baecc359faa955c52f4e4348da2b
Mark Kurpiel <MarkKurpiel@gmail.com>
2023-10-05 19:58:30 -0700

    added tailwind aspect ratio and mad resum adaptive

--- components/Resume.js
+++ components/Resume.js
@@ -3,10 +3,8 @@ import React, { useEffect, useState } from "react";
 export default function Resume() {
   return (
     <section>
-      <div className="relative z-10 rounded-md shadow-md bg-[#202A44] p-4 md:p-10 lg:p-20 max-w-6xl mx-auto mb-20 -mt-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <iframe className="w-full aspect-video" src="/MarkKurpielResume.pdf" style={{ height: '100vh', width: '70vh' }}/>
-        </div>
+      <div className="relative z-10 rounded-md shadow-md bg-[#202A44] p-4 md:p-10 lg:p-20 max-w-6xl mx-auto mb-20">
+            <iframe className="w-full h-screen aspect-video" src="/MarkKurpielResume.pdf"/>
       </div>
     </section>
   );
