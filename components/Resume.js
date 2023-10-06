a2894b9150b5f7c8c4931f55d30ecbdcaaa1c4e9
Mark Kurpiel <MarkKurpiel@gmail.com>
2023-10-05 19:48:16 -0700

    resume width fix

--- components/Resume.js
+++ components/Resume.js
@@ -5,7 +5,7 @@ export default function Resume() {
     <section>
       <div className="relative z-10 rounded-md shadow-md bg-[#202A44] p-4 md:p-10 lg:p-20 max-w-6xl mx-auto mb-20 -mt-4">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <iframe className="w-full aspect-video" src="/MarkKurpielResume.pdf" style={{ height: '100vh', width: '100vh' }}/>
+            <iframe className="w-full aspect-video" src="/MarkKurpielResume.pdf" style={{ height: '100vh', width: '70vh' }}/>
         </div>
       </div>
     </section>
